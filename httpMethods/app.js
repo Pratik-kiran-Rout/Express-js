e8e1ea0c61ed3e4b9a5b6e0a695ffc198af1ab97
Pratik <pratikkiranrout@outlook.com>
2025-06-08 12:19:42 +0530

    feat: improve form data handling and enhance error messages in API

--- httpMethods/app.js
+++ httpMethods/app.js
@@ -6,24 +6,35 @@ let {people} = require('./data');
 // Middleware built-in
 app.use(express.static('./methods-public'));
 
-// Middleware for get the data from the form
+// Middleware for get the data from the form (parse from data)
 app.use(express.urlencoded({extended:false}))
+
+// parse json
+app.use(express.json()) 
+//! express.json() is a middleware function
 
 // People API we just read the data 
 app.get('/api/people',(req,res)=>{
      res.status(200).json({success:true, data:people});
 })
+
 //here we actully add the data
 app.post('/api/people',(req,res)=>{
-res.status(201).send('')
+     const {name} = req.body
+     if(!name){
+          return res.status(400).json({success:false, msg:'please provide name value'})
+     }
+res.status(201).json({success:true,person:name});
 })
+// here by using post method we add the data  
+
 app.post('/login',(req,res)=>{
      // console.log(req.body); it recieves data from the form 
      const {name} = req.body;
      if(name){
           return res.status(200).send(`Welcome ${name}`);
      }else{
-          return res.status(401).send('Please provide the name');
+          return res.status(401).send('Please Provide Credentials !');
      }  
 })
 
@@ -31,10 +42,9 @@ app.listen(5000,()=>{
      console.log("server is running in the port 5000 ...");
 })
 
- 
                    //! Express.urlencoded()
 
-// ! app.use(express.urlencoded({extended:false})) is used to parse the incoming request bodies in a middleware before your handlers, available under the req.body property.
+//! app.use(express.urlencoded({extended:false})) is used to parse the incoming request bodies in a middleware before your handlers, available under the req.body property.
 
 //! in simple it is used to parse the form data from the request body and make it available in req.body object.
 
